354ea7a1bfbf79cd8a0eb0166fe2a3d89ae4d5ab
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-03-07 23:51:34 +0100

    feat: design the stack section

---
+++ components/common/stackItem.tsx
@@ -0,0 +1,18 @@
+import React, { PropsWithChildren } from 'react';
+
+type StackItemProps = {
+  title: string;
+};
+
+const StackItem = ({ children, title }: PropsWithChildren<StackItemProps>) => {
+  return (
+    <div className="cursor-default">
+      <div className="flex items-center justify-center flex-col bg-white shadow-sm rounded-xl p-4">
+        <div className="flex items-center justify-center flex-shrink-0 h-20 w-20 rounded-xl">{children}</div>
+        <div className="text-lg font-bold mt-3">{title}</div>
+      </div>
+    </div>
+  );
+};
+
+export default StackItem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,14 @@ import Layout from '@/components/layout';
 import Intro from '@/components/sections/intro';
 import Experience from '@/components/sections/experience';
 import Education from '@/components/sections/education';
+import Stack from '@/components/sections/stack';
 
 const IndexPage = () => (
   <Layout title="Eric Cabrel TIOGO">
     <Intro />
     <Experience />
     <Education />
+    <Stack />
   </Layout>
 );
 
