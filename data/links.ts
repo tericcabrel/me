e1782d97e9051da245210c86828fddf3d3a20f64
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-03-07 18:23:30 +0100

    feat: design the first section

--- data/links.ts
+++ data/links.ts
@@ -1,4 +1,8 @@
-import { HeaderLinkType } from '../types';
+import { HeaderLinkType, SocialLinkType } from '../types';
+import LinkedinIcon from '@/components/icons/linkedin';
+import GithubIcon from '@/components/icons/github';
+import TwitterIcon from '@/components/icons/twitter';
+import EmailIcon from '@/components/icons/email';
 
 export const links: HeaderLinkType[] = [
   { name: 'Experience', path: '#experience' },
@@ -6,4 +10,11 @@ export const links: HeaderLinkType[] = [
   { name: 'Stack', path: '#stack' },
   { name: 'Blog', path: '#blog' },
   { name: 'Contact', path: '#contact' },
+];
+
+export const socialLinks: SocialLinkType[] = [
+  { content: GithubIcon, path: 'https://github.com/tericcabrel', width: 32, height: 32 },
+  { content: LinkedinIcon, path: 'https://linkedin.com/in/tericcabrel' },
+  { content: TwitterIcon, path: 'https://twitter.com/tericcabrel', width: 32, height: 32 },
+  { content: EmailIcon, path: 'mailto:tericcabrel@yahoo.com', width: 36, height: 36 },
 ];
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,18 @@
+import { FC } from 'react';
+
 export type HeaderLinkType = {
   name: string;
   path: string;
+};
+
+export type IconProps = {
+  width?: number;
+  height?: number;
+};
+
+export type SocialLinkType = {
+  content: FC<IconProps>;
+  path: string;
+  width?: number;
+  height?: number;
 };
